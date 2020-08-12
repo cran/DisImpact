48dc594b92123e64e64ce410a588a8b0b4c1a1e3
Vinh Nguyen <nguyenvq714@gmail.com>
2020-08-12 10:32:16 +0000

    version 0.0.12

--- inst/doc/Scaling-DI-Calculations.R
+++ inst/doc/Scaling-DI-Calculations.R
@@ -23,7 +23,7 @@ df_di_summary <- di_iterate(data=student_equity
 
 ## df_di_summary <- di_iterate(data=student_equity, success_vars=c('Math', 'English', 'Transfer'), group_vars=c('Ethnicity', 'Gender'), scenario_repeat_by_vars=c('Ed_Goal', 'College_Status'))
 
-## df_di_summary <- di_iterate(data=student_equity, success_vars=c('Math', 'English', 'Transfer'), group_vars=c('Ethnicity', 'Gender'), cohort_vars=c('Cohort_Math', 'Cohort_English', 'Cohort'), scenario_repeat_by_vars=c('Ed_Goal', 'College_Status'), ppg_reference_groups=c('White', 'Male'), di_80_index_reference_groups=c('White', 'Male'))
+## df_di_summary_2 <- di_iterate(data=student_equity, success_vars=c('Math', 'English', 'Transfer'), group_vars=c('Ethnicity', 'Gender'), cohort_vars=c('Cohort_Math', 'Cohort_English', 'Cohort'), scenario_repeat_by_vars=c('Ed_Goal', 'College_Status'), ppg_reference_groups=c('White', 'Male'), di_80_index_reference_groups=c('White', 'Male'))
 
 ## df_di_summary <- di_iterate(data=student_equity, success_vars=c('Math', 'English', 'Transfer'), group_vars=c('Ethnicity', 'Gender'), cohort_vars=c('Cohort_Math', 'Cohort_English', 'Cohort'), scenario_repeat_by_vars=c('Ed_Goal', 'College_Status'), ppg_reference_groups=c('all but current'), di_80_index_reference_groups=c('White', 'Male'))
 
@@ -51,7 +51,7 @@ df_di_summary %>%
   as.data.frame %>% 
   kable
 
-## ----results=FALSE-------------------------------------------------------
+## ------------------------------------------------------------------------
 # No Disaggregation
 df_di_summary %>%
   filter(Ed_Goal=='- All', College_Status=='- All', success_variable=='Math', disaggregation=='- None') %>%
@@ -162,4 +162,120 @@ if (run_plots) {
     ggtitle('Dashboard drop-down selections:', subtitle=paste0("Ed Goal = 'Deg/Transfer' | College Status = '- All' | Outcome = 'English' | Disaggregation = 'Gender'"))
   ggsave('Dashboard_4.png', height=5, width=9)
 }
+
+## ------------------------------------------------------------------------
+args(di_iterate)
+
+## ------------------------------------------------------------------------
+dim(student_equity)
+
+## Example summarized data set
+student_equity_summ <- student_equity %>%
+  group_by(Ethnicity, Gender, Cohort, Cohort_Math, Cohort_English, Ed_Goal, College_Status) %>%
+  summarize(N=n() %>% as.numeric # not needed, for all.equal()
+            , Math=sum(Math, na.rm=TRUE)
+            , English=sum(English, na.rm=TRUE)
+            , Transfer=sum(Transfer, na.rm=TRUE)
+            ) %>%
+  ungroup
+
+dim(student_equity_summ) # same number of columns, less number of rows
+
+student_equity_summ %>% head %>% as.data.frame # first few rows
+
+## Run on summarized data set
+df_di_summary_2 <- di_iterate(data=student_equity_summ
+                          , success_vars=c('Math', 'English', 'Transfer')
+                          , group_vars=c('Ethnicity', 'Gender')
+                          , cohort_vars=c('Cohort_Math', 'Cohort_English', 'Cohort')
+                          , scenario_repeat_by_vars=c('Ed_Goal', 'College_Status')
+                          , weight_var='N' # SET THIS
+                            )
+dim(df_di_summary)
+dim(df_di_summary_2) # more rows?  because of NA cohort
+dim(df_di_summary_2 %>% filter(!is.na(cohort)))
+
+## ## if user wants to see the extra rows
+## extra_rows <- df_di_summary_2 %>%
+##   anti_join(df_di_summary %>% select(Ed_Goal, College_Status, success_variable, cohort_variable, cohort, disaggregation, group))
+## difference %>% head %>% as.data.frame  
+
+all.equal(df_di_summary
+        , df_di_summary_2 %>% filter(!is.na(cohort))
+          ) # returned results are the same
+
+
+## ------------------------------------------------------------------------
+df_di_summary_2 <- di_iterate(data=student_equity
+                          , success_vars=c('Math', 'English', 'Transfer')
+                          , group_vars=c('Ethnicity', 'Gender')
+                          , cohort_vars=c('Cohort_Math', 'Cohort_English', 'Cohort')
+                          , scenario_repeat_by_vars=c('Ed_Goal', 'College_Status')
+                          , include_non_disagg_results=FALSE ## SET THIS
+                            )
+dim(df_di_summary)
+dim(df_di_summary_2) ## less rows because no longer have disaggregated results
+table(df_di_summary_2$disaggregation) # No more '- None'
+
+## ------------------------------------------------------------------------
+df_di_summary_2 <- di_iterate(data=student_equity
+                            , success_vars=c('Math', 'English', 'Transfer')
+                            , group_vars=c('Ethnicity', 'Gender')
+                            , cohort_vars=c('Cohort_Math', 'Cohort_English', 'Cohort')
+                            , scenario_repeat_by_vars=c('Ed_Goal', 'College_Status')
+                            , ppg_reference_groups=c('White', 'Male') ## corresponds to each variable in group_vars
+                              )
+
+## ------------------------------------------------------------------------
+df_di_summary_2 <- di_iterate(data=student_equity
+                            , success_vars=c('Math', 'English', 'Transfer')
+                            , group_vars=c('Ethnicity', 'Gender')
+                            , cohort_vars=c('Cohort_Math', 'Cohort_English', 'Cohort')
+                            , scenario_repeat_by_vars=c('Ed_Goal', 'College_Status')
+                            , di_prop_index_cutoff=0.9 # Easier to declare DI using PI
+                              )
+
+## ------------------------------------------------------------------------
+df_di_summary_2 <- di_iterate(data=student_equity
+                            , success_vars=c('Math', 'English', 'Transfer')
+                            , group_vars=c('Ethnicity', 'Gender')
+                            , cohort_vars=c('Cohort_Math', 'Cohort_English', 'Cohort')
+                            , scenario_repeat_by_vars=c('Ed_Goal', 'College_Status')
+                            , di_80_index_reference_groups=c('White', 'Male') ## corresponds to each variable in group_vars
+                              )
+
+## ------------------------------------------------------------------------
+df_di_summary_2 <- di_iterate(data=student_equity
+                            , success_vars=c('Math', 'English', 'Transfer')
+                            , group_vars=c('Ethnicity', 'Gender')
+                            , cohort_vars=c('Cohort_Math', 'Cohort_English', 'Cohort')
+                            , scenario_repeat_by_vars=c('Ed_Goal', 'College_Status')
+                            , di_80_index_cutoff=0.5 # Harder to declare DI using 80% index
+                              )
+
+## ------------------------------------------------------------------------
+# Multiple group variables and different reference groups
+df_di_summary_long <- bind_rows(
+  di_iterate(data=student_equity
+           , success_vars=c('Math', 'English', 'Transfer')
+           , group_vars=c('Ethnicity', 'Gender')
+           , cohort_vars=c('Cohort_Math', 'Cohort_English', 'Cohort')
+           , scenario_repeat_by_vars=c('Ed_Goal', 'College_Status')
+             )
+  , di_iterate(data=student_equity
+           , success_vars=c('Math', 'English', 'Transfer')
+           , group_vars=c('Ethnicity', 'Gender')
+           , cohort_vars=c('Cohort_Math', 'Cohort_English', 'Cohort')
+           , scenario_repeat_by_vars=c('Ed_Goal', 'College_Status')
+           , ppg_reference_groups=c('White', 'Male') ## corresponds to each variable in group_vars
+           , include_non_disagg_results = FALSE # Already have non-disaggregated results in the first run
+             )
+)
+
+dim(df_di_summary_long)
+
+## ------------------------------------------------------------------------
+## df_di_summary %>%
+##   mutate(FERPA_Block=ifelse(n < 10, 1, 0)) %>%
+##   filter(FERPA_Block == 0)
 
